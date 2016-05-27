ca4da1c3347b5fcbde6482e8505fafdf6e264a59
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2016-05-28 08:01:13 +0900

    punchとmarginの追加

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include "STM_BLDCMotor.h"
 
 #define GAIN 10.0
+#define PUNCH 0.15
+#define MARGIN 0.003
+#define MAX_ANGLE 30.0
+#define MIN_ANGLE -30.0
+#define OFFSET_ANGLE 2.3
 #define BAUDRATE 115200
 #define OFFSET 2.3;
 
@@ -35,7 +40,12 @@ struct RobotStatus{
   float target_angle;
   float current_angle;
   float gain;
+  float punch;
+  float margin;
   float max_torque;
+  float max_angle;
+  float min_angle;
+  float offset_angle;
   bool is_servo_on;
   bool change_target;
   bool isWakeupMode;
@@ -48,7 +58,12 @@ void initialize()
   status.target_angle = OFFSET;
   status.current_angle = 0;
   status.gain = GAIN;
+  status.punch  = PUNCH;
+  status.margin = MARGIN;
   status.max_torque = 1.0;
+  status.max_angle = MAX_ANGLE;
+  status.min_angle = MIN_ANGLE;
+  status.offset_angle = OFFSET_ANGLE;
   status.is_servo_on = false;
   status.led_state = 0;
   status.led_count = 0;
@@ -79,8 +94,8 @@ int main() {
       led2 = led2 ^ 1;
       int address, data;
       if (commnand_parser.getNextCommand(&address, &data) > 0){
-        data = max(min(data, 3000), -3000);
-        status.target_angle = (float)data / 18000.0 * M_PI + OFFSET;
+        data = max(min(data, status.max_angle * 100), status.min_angle * 100);
+        status.target_angle = (float)data * M_PI / 18000.0  + status.offset_angle;
         motor.status_changed();
       }
     }
@@ -88,8 +103,18 @@ int main() {
 
 		status.current_angle = as5600;
 		if (as5600.getError()) break;
-		float val = max(min(status.gain * (status.current_angle - status.target_angle),
-      status.max_torque), -status.max_torque);
+    float error = status.current_angle - status.target_angle;
+    float pwm = 0;
+    if (fabs(error) > status.margin){
+      if (error > 0){
+        error -= status.margin;
+        pwm = status.gain * error + status.punch;
+      } else {
+        error += status.margin;
+        pwm = status.gain * error - status.punch;
+      }
+    }
+		float val = max(min(pwm, status.max_torque), -status.max_torque);
 		motor = val;
     
     wait(0.001);

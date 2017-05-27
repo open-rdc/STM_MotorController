3b423e5a6bef99a284c5ada2e3d75bd7e0528b10
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2017-05-27 11:17:34 +0900

    角度の挙動の計測

---
+++ read_servo/read_servo.bat
@@ -0,0 +1,14 @@
+read_servo1.ttl
+read_servo2.ttl
+read_servo3.ttl
+read_servo4.ttl
+read_servo5.ttl
+read_servo6.ttl
+read_servo7.ttl
+read_servo8.ttl
+read_servo9.ttl
+read_servo10.ttl
+read_servo11.ttl
+read_servo12.ttl
+
+
